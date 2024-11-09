ea5e95571ca117282c7e01b824629023ec0afcd4
Augusto Zanoli <zancamar9@gmail.com>
2024-11-09 19:42:41 -0300

    criação do projeto

---
+++ aprendizado_flutter/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    Container(
+      child: Text(
+        'Aprendizado',s
+        textDirection: TextDirection.ltr,
+        style: TextStyle(color: Colors.white, fontSize: 50.0),
+      )
+    )
+  );
+}
+
